87255b0e98c923ad474ebde4064b443030efa57a
Jaime Salebe <jaimesalebe@outlook.com>
2024-02-29 17:00:21 -0500

    add:  redirect

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [{
+            source: '/',
+            destination: '/books',
+            permanent: true
+        }]
+    }
 };
 
 export default nextConfig;
